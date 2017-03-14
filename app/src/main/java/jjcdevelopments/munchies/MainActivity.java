dec3eb89cac4e0f2917a1b72bb54a832ff220a73
BlackMagi018 <Hawkman063@aol.com>
2017-03-13 21:51:55 -0400

    More layouts

--- app/src/main/java/jjcdevelopments/munchies/MainActivity.java
+++ app/src/main/java/jjcdevelopments/munchies/MainActivity.java
@@ -1,7 +1,9 @@
 package jjcdevelopments.munchies;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Called when the user taps the Login button
+     */
+    public void SearchPageView(View view) {
+        Intent SearchPage = new Intent(getApplicationContext(), FeedMeActivity.class);
+
+        startActivity(SearchPage);
     }
 }
